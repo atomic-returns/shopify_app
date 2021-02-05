ee2237e8c1f77c9e006c896b9d5d7d96254eaeeb
Rezaan Syed <rezaan.syed@shopify.com>
2021-02-05 17:52:18 -0500

    Spike GraphQL, REST and database performance times

--- app/controllers/concerns/shopify_app/scopes_verification.rb
+++ app/controllers/concerns/shopify_app/scopes_verification.rb
@@ -18,7 +18,11 @@ module ShopifyApp
     end
 
     def current_merchant_scopes
-      ShopifyApp::SessionRepository.retrieve_shop_scopes(current_shopify_domain)
+      measure_performance "fetch_scopes_via_rest.txt" do
+        #installed_access_scopes_via_db
+        #installed_access_scopes_via_graphql
+        installed_access_scopes_via_rest
+      end
     end
 
     def configuration_scopes
@@ -26,6 +30,59 @@ module ShopifyApp
     end
 
     private
+
+    def measure_performance(filename)
+      start = Time.now
+      access_scopes = yield
+      finish = Time.now
+
+      f = File.new(filename, 'a')
+      f << "#{finish - start}\n"
+      f.close
+
+      access_scopes
+    end
+
+    def installed_access_scopes_via_db
+      ShopifyApp::SessionRepository.retrieve_shop_scopes(current_shopify_domain)
+    end
+
+    def installed_access_scopes_via_rest
+      begin
+        shop_session = ShopifyApp::SessionRepository.retrieve_shop_session_by_shopify_domain(current_shopify_domain)
+        ShopifyAPI::Base.activate_session(shop_session)
+
+        ShopifyAPI::AccessScope.find(:all).map(&:handle)
+      ensure
+        ShopifyAPI::Base.clear_session
+      end
+    end
+
+    def installed_access_scopes_via_graphql
+      # This is identical to LoginProtection right now
+      shop_session = ShopifyApp::SessionRepository.retrieve_shop_session_by_shopify_domain(current_shopify_domain)
+      ShopifyAPI::Base.activate_session(shop_session)
+
+      client = ShopifyAPI::GraphQL.client(ShopifyApp.configuration.api_version)
+
+      # This is the real work
+      result = client.query(client.parse(installation_query))
+
+      # This looks like a ScopeUtilities method
+      result.data.app_installation.access_scopes.map { |access_scope| access_scope.handle }
+    end
+
+    def installation_query
+      query = <<-GRAPHQL
+        {
+          appInstallation {
+            accessScopes {
+              handle
+            }
+          }
+        }
+      GRAPHQL
+    end
 
     def current_shopify_domain
       return if params[:shop].blank?
